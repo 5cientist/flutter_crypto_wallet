ed885b9b2625ffdaee214177e7b11bd66eb34c4b
hanshir <hanshirlukku@gmail.com>
2020-10-21 13:53:47 +0530

    added two buttons

--- lib/ui/authentication.dart
+++ lib/ui/authentication.dart
@@ -49,8 +49,30 @@ class _AuthenticationState extends State<Authentication> {
                 ),
               ),
             ),
-            Container(),
-            Container()
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Register"),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              height: 45,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text("Log In"),
+              ),
+            )
           ],
         ),
       ),
